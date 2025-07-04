0052f9deb463d97cea79be6e8c3e16a72223d9f2
PLGVS <pedro.gaspar1214@gmail.com>
2025-07-04 13:55:07 -0300

    Project created

---
+++ src/main/java/com/plgvs/individual_challenge_bagaggio/IndividualChallengeBagaggioApplication.java
@@ -0,0 +1,13 @@
+package com.plgvs.individual_challenge_bagaggio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualChallengeBagaggioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualChallengeBagaggioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plgvs/individual_challenge_bagaggio/IndividualChallengeBagaggioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plgvs.individual_challenge_bagaggio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualChallengeBagaggioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

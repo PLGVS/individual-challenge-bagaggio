43762deb064a2cc9d6f26875c80dba140286af9e
PLGVS <pedro.gaspar1214@gmail.com>
2025-07-04 14:27:48 -0300

    feat: GET endpoint to find a product by id

--- src/main/java/com/plgvs/individual_challenge_bagaggio/resources/ProdutoResource.java
+++ src/main/java/com/plgvs/individual_challenge_bagaggio/resources/ProdutoResource.java
@@ -5,10 +5,12 @@ import com.plgvs.individual_challenge_bagaggio.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/produtos")
@@ -22,5 +24,12 @@ public class ProdutoResource {
         List<Produto> produtos = produtoService.findAll();
 
         return ResponseEntity.ok().body(produtos);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Produto>> findById(@PathVariable Long id){
+        Optional<Produto> produto = produtoService.findById(id);
+
+        return ResponseEntity.ok().body(produto);
     }
 }
